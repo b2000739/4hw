40315ede773c76b1e78e3b7b0ba6c0f5f8d383dc
b2000739 <113001869+b2000739@users.noreply.github.com>
2022-12-28 10:42:15 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $sum = 0;
 $massive = array(-2, -4, 5, 4, 5, -7);
 for ($i = 0; $i < count($massive); $i++) {
-	if ($massive[$i] < 0 and $massive[$i] % 2 == 0) {
+	if ($massive[$i] < 0 && $massive[$i] % 2 == 0) {
 		$sum += $massive[$i];
 	}
 }
